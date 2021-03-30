5b8bf9d9689c6b0d2291c974153c509f57ec0ae4
Mohammed <atefhamosa@gmail.com>
2021-03-30 19:02:53 +0200

    Make getData function

--- src/components.js
+++ src/components.js
@@ -5,4 +5,25 @@ const text = (elem, text, className) => {
     return t;
 };
 
-export { text };
+const getData = (place) => {
+    let weather={};
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&lang=en&appid=96234945f21abe7a841c8fc0306c59be`)
+        .then(function(response) { 
+            response.json()
+            .then(function (data) {
+                weather.country =  data.name
+                weather.sky =  data.weather[0].description
+                weather.icon =  data.weather[0].icon
+                weather.windSpeed =  data.wind.speed
+                weather.windDirection =  data.wind.deg
+                weather.maxTemp =  data.main.temp_max
+                weather.minTemp =  data.main.temp_min
+            })
+        })
+        .catch(function(err) {
+            return err
+        });
+    return weather
+}
+
+export { text, getData };
--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 import * as components from './components'
 
+const content = document.getElementById('content')
+const head = components.text('h1', 'Weather', 'head')
+const label = components.text('label', 'Enter country name:', 'label')
+const input = components.text('input', '', 'input')
+const btn = components.text('button', 'Get', 'button')
+const container = components.text('div', '', 'container')
+
+content.appendChild(head)
+content.appendChild(label)
+content.appendChild(input)
+content.appendChild(btn)
+content.appendChild(container)
+
+btn.onclick = async () => {
+    container.innerHTML = ''
+    let country = input.value
+    let weather = components.getData(country)
+    console.log(weather)
+
+    let name = components.text('h2', weather, 'name')
+    container.appendChild(name)
+}
+
+
+// container
