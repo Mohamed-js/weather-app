54537ca8a9aa196427cd5ba936a61441873b72a8
Mohammed <atefhamosa@gmail.com>
2021-03-31 16:33:17 +0200

    Make the View logic

--- src/components.js
+++ src/components.js
@@ -5,20 +5,20 @@ const text = (elem, text, className) => {
     return t;
 };
 
-const getData = (place) => {
+const getData = async(place) => {
     let weather={};
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&lang=en&appid=96234945f21abe7a841c8fc0306c59be`)
+    await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&lang=en&appid=96234945f21abe7a841c8fc0306c59be`)
         .then(function(response) { 
-            response.json()
-            .then(function (data) {
-                weather.country =  data.name
-                weather.sky =  data.weather[0].description
-                weather.icon =  data.weather[0].icon
-                weather.windSpeed =  data.wind.speed
-                weather.windDirection =  data.wind.deg
-                weather.maxTemp =  data.main.temp_max
-                weather.minTemp =  data.main.temp_min
-            })
+            return response.json()
+        })
+        .then(function (data) {
+            weather.country =  data.name
+            weather.sky =  data.weather[0].description
+            weather.icon =  data.weather[0].icon
+            weather.windSpeed =  data.wind.speed
+            weather.windDirection =  data.wind.deg
+            weather.maxTemp =  data.main.temp_max
+            weather.minTemp =  data.main.temp_min
         })
         .catch(function(err) {
             return err
@@ -26,4 +26,11 @@ const getData = (place) => {
     return weather
 }
 
-export { text, getData };
+function appendAll() {
+    const cont = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        cont.appendChild(arguments[i])
+    }
+}
+
+export { text, getData, appendAll };   
--- src/index.js
+++ src/index.js
@@ -4,24 +4,32 @@ const content = document.getElementById('content')
 const head = components.text('h1', 'Weather', 'head')
 const label = components.text('label', 'Enter country name:', 'label')
 const input = components.text('input', '', 'input')
-const btn = components.text('button', 'Get', 'button')
+const btn = components.text('a', 'Get Weather', 'button')
 const container = components.text('div', '', 'container')
+const load = components.text('img', '', 'c-img');
+load.src = './imgs/load.gif';
 
-content.appendChild(head)
-content.appendChild(label)
-content.appendChild(input)
-content.appendChild(btn)
-content.appendChild(container)
+components.appendAll(content, head, label, input, btn, container)
 
 btn.onclick = async () => {
+    
+    // Empty container and make loading icon
     container.innerHTML = ''
+    container.appendChild(load)
+
+    // Get and store the data 
     let country = input.value
-    let weather = components.getData(country)
-    console.log(weather)
+    let weather = await components.getData(country);
 
-    let name = components.text('h2', weather, 'name')
-    container.appendChild(name)
+    // Empty container from loading and get img src
+    container.innerHTML = ''
+    let img = components.text('img', '', 'c-img') ;
+    img.src = `http://openweathermap.org/img/wn/${weather.icon}@4x.png`;
+
+    // Append weather info
+    components.appendAll(container, components.text('h2', weather.country, 'c-name'), img)
+    components.appendAll(container, components.text('label', 'Max-Temp', 'c-max'), components.text('h4', weather.maxTemp + '°', 'c-max'))
+    components.appendAll(container, components.text('label', 'Min-Temp', 'c-min'), components.text('h4', weather.minTemp + '°', 'c-min'))
+    components.appendAll(container, components.text('label', 'Wind Speed', 'c-speed'), components.text('h6', weather.windSpeed, 'c-speed'))
+    components.appendAll(container, components.text('label', 'Wind Direction', 'c-direction'), components.text('h6', weather.windDirection + 'deg', 'c-direction'))
 }
-
-
-// container
